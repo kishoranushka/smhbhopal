a0387be8130b1817da6c04e37983f831f8e0326d
Anu <anuskhka@123>
2024-04-16 23:20:18 +0530

    fixed the navbar changing color on going to back to top

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
 const navbar = document.querySelector(".navWrapContainer");
 let start = navbar.offsetTop;
-// console.log(navbar);
 let originalBackgroundColor = window
   .getComputedStyle(navbar)
   .getPropertyValue("background-color");
-// console.log(originalBackgroundColor);
 let image = document.querySelector(".navContainerLeft img");
-// console.log(image);
 
 const texttoChange = document.querySelector(".navContainerRight");
-// console.log(texttoChange);
 
 const btn = document.querySelector(".nCRT button");
-// console.log(btn);
+let btnColor = window
+  .getComputedStyle(btn)
+  .getPropertyValue("background-color");
 
-// let textColor = window.getComputedStyle(color).getPropertyValue("color");
-// console.log(textColor);
-
-// console.log(start);
 function stickynavbar() {
-  if (window.scrollY >= start) {
+  console.log(start);
+  if (window.scrollY > start) {
     navbar.classList.add("sticky");
     navbar.style.backgroundColor = "white";
     image.src = "assets/smh-logo-color.png";
     texttoChange.style.color = "black";
     btn.style.background = "linear-gradient(to right, #4cb848, #aed035)";
-  } else  {
+  } else {
     navbar.classList.remove("sticky");
-    navbar.style.backgroundColor = "red";
+    navbar.style.backgroundColor = originalBackgroundColor;
+    image.src = "assets/smh-logo-white.png";
+    texttoChange.style.color = "white";
+    btn.style.background = btnColor;
   }
 }
 window.addEventListener("scroll", stickynavbar);
